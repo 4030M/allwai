51b98a1a1fa8b462c5d171170ccc78dafda8f3b8
Ali H. Al-Kabbi <43940510+xiAllwai@users.noreply.github.com>
2018-10-27 20:49:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ if(!client) var client = new Discord.Client();
 if(!prefix) var prefix = "#";
 
 const adminprefix = "#";
-const devs = ['195088897234042880','505762258857492490'];
+const devs = ['195088897234042880'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
