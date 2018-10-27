88a7c079f43fab8d71d81f8de33d4cd46288e750
Ali H. Al-Kabbi <43940510+xiAllwai@users.noreply.github.com>
2018-10-27 20:57:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,30 +29,3 @@ if (message.content.startsWith(adminprefix + 'stream')) {
 }
 });
 client.login(process.env.BOT_TOKEN);
-
-const devs = ['505762258857492490'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-if (message.content.startsWith(adminprefix + 'playing')) {
-  client.user.setGame(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | The Playing Status Has Been Changed To : ${argresult}**`).then(message => {message.delete(6000)})
-} else 
-  if (message.content.startsWith(adminprefix + 'setstatus')) {
-client.user.setStatus(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | The Bot Status Has Been Changed To : ${argresult}**`).then(message => {message.delete(6000)})
-} else
-  if (message.content.startsWith(adminprefix + 'watch')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark: | The Watch Status Has Been Changed To : ${argresult}**`).then(message => {message.delete(6000)})
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`:white_check_mark: | The Listen Status Has Been Changed To : ${argresult}**`).then(message => {message.delete(6000)})
-  } else 
-if (message.content.startsWith(adminprefix + 'stream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk")
-    message.channel.sendMessage(`**:white_check_mark: | The Stream Bot Has Been Changed To : ${argresult}**`).then(message => {message.delete(6000)})
-}
-});
-client.login(process.env.BOT_TOKEN2);
