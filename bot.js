27db0f1eb50fcea70817c00dc0ad0b923c1af25c
Ali H. Al-Kabbi <43940510+xiAllwai@users.noreply.github.com>
2018-12-12 23:47:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,30 +29,3 @@ if (message.content.startsWith(adminprefix + 'st')) {
 }
 });
 client.login(process.env.BOT_TOKEN);
-
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-if (message.content.startsWith(adminprefix + 'pl')) {
-  client.user.setGame(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
-} else 
-  if (message.content.startsWith(adminprefix + 'status')) {
-client.user.setStatus(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
-} else
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
-  } else 
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk")
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
-}
-});
-
-client.login(process.env.BOT_TOKEN2);
