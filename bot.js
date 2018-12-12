8aedc491dbd9096a691d8fa73479ea0cf037c49f
Ali H. Al-Kabbi <43940510+xiAllwai@users.noreply.github.com>
2018-12-12 23:56:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,23 +9,23 @@ client.on('message', message => {
     if (!devs.includes(message.author.id)) return;
 if (message.content.startsWith(adminprefix + 'pl')) {
   client.user.setGame(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+    message.channel.sendMessage(`**:white_check_mark: | Playing : ${argresult}**`).then(message => {message.delete(6000)})
 } else 
   if (message.content.startsWith(adminprefix + 'status')) {
 client.user.setStatus(argresult)
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+    message.channel.sendMessage(`**:white_check_mark: | Set Status to : ${argresult}**`).then(message => {message.delete(6000)})
 } else
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+      message.channel.sendMessage(`**:white_check_mark: | Watching : ${argresult}**`).then(message => {message.delete(6000)})
   } else 
   if (message.content.startsWith(adminprefix + 'lis')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+      message.channel.sendMessage(`**:white_check_mark: | Lisitening to : ${argresult}**`).then(message => {message.delete(6000)})
   } else 
 if (message.content.startsWith(adminprefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/allwai511")
-    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+    message.channel.sendMessage(`**:white_check_mark: | Streaming : ${argresult}**`).then(message => {message.delete(6000)})
 }
 });
 client.login(process.env.BOT_TOKEN);
